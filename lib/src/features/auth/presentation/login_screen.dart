14d89d2d1b4454be6cf7033b8e68f8ec7c5972dc
Muhammad Vaid <molvy32@gmail.com>
2025-01-28 17:58:08 +0200

    fixed error

--- lib/src/features/auth/presentation/login_screen.dart
+++ lib/src/features/auth/presentation/login_screen.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-class LoginScreen extends ConsumerWidget {
+class LoginScreen extends HookConsumerWidget {
   const LoginScreen({super.key});
 
   @override
