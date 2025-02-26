14c0410e552b552adf1becbd7cccdd655122039e
fatoom9 <fatima.abureesh2001@gmail.com>
2025-02-26 07:41:49 -1200

    implement sealed class

---
+++ lib/src/exceptions/app_exceptions.dart
@@ -0,0 +1,34 @@
+//base class
+sealed class AppException implements Exception {
+  AppException(this.code, this.message);
+  final String code;
+  final String message;
+  @override
+  String toString() => '$code: $message';
+}
+
+String fn(AppException exception) {
+  return switch (exception) {
+    EmailAlreadyInUseException() => 'Email already in use',
+    WeakPasswordException() => 'Password is too weak',
+    WrongPasswordException() => 'Wrong password',
+    UserNotFoundException() => 'User not found',
+  };
+}
+
+class EmailAlreadyInUseException extends AppException {
+  EmailAlreadyInUseException()
+      : super('email-already-in-use', 'Email already in use');
+}
+
+class WeakPasswordException extends AppException {
+  WeakPasswordException() : super('weak-password', 'Password is too weak');
+}
+
+class WrongPasswordException extends AppException {
+  WrongPasswordException() : super('wrong-password', 'Wrong password');
+}
+
+class UserNotFoundException extends AppException {
+  UserNotFoundException() : super('user-not-found', 'User not found');
+}
